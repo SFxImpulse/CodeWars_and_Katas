fb6482fcbd84b11e101b43cbf4dea3ad1802b300
David Braverman <braverman.david.business@gmail.com>
2023-02-13 21:42:01 -0500

    add another python kata.

---
+++ Katas/MinimumAndMaximum.py
@@ -0,0 +1,11 @@
+# Create two functions, one that finds the maximum value in an array, and one that finds the minimum value in an array
+
+maximum = lambda arr: max(arr)
+
+minimum = lambda arr: min(arr)
+
+# Refactored
+
+maximum = max
+
+minimum = min
