93c2b823669630d7aa8e0f8c7b21538b5cc51898
David Braverman <braverman.david.business@gmail.com>
2023-05-19 21:41:03 -0400

    add another kata

---
+++ Katas/twoSort.js
@@ -0,0 +1,3 @@
+// Given an array of strings, sort the array alphabetically, and return the first value with "***" between each character:
+
+const twoSort = arr => arr.sort()[0].split("").Join("***");
