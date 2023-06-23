d6f4f64a7dd369753a4b9f6fe51e27faa7a674c5
David Braverman <braverman.david.business@gmail.com>
2023-06-22 20:37:37 -0400

    add another kata

--- Katas/HowManyLightsabersDoYouOwn.py
+++ Katas/HowManyLightsabersDoYouOwn.py
@@ -1,3 +1,5 @@
 # Given a string, return 18 if that string is "Zach", otherwise return 0:
 
 how_many_light_sabers_do_you_own = lambda name: 18 if name == "Zach" else 0
+
+# Test on codewars failed because one of the tests did not include a function parameter, but the function still works as intended if you actually add all the arguments.
