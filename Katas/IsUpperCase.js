28f80321127317620f0e0b6f193a2c2879950ada
David Braverman <braverman.david.business@gmail.com>
2023-07-31 20:11:27 -0400

    add another kata

--- Katas/IsUpperCase.js
+++ Katas/IsUpperCase.js
@@ -1,5 +1,5 @@
 // Given a string, return true if the entire string is uppercase, else return false:
 
 String.prototype.isUpperCase = function() {
-  return this == this.toUpperCase() ? true : false;
+  return this == this.toUpperCase();
 }
