37f5f818d6a30b99a77ce2b496aceb83e0344f80
David Braverman <braverman.david.business@gmail.com>
2023-08-24 20:30:06 -0400

    add another kata

--- Katas/Greetings.ts
+++ Katas/Greetings.ts
@@ -1,3 +1,7 @@
 const greeting = (name: string) => console.log(`Hello, ${name}!`);
 
 greeting("Dana");
+
+// Refactored:
+
+const greetingV2 = (name: string = ''): string => `Hello, ${!name ? 'World': name[0].toUpperCase() + name.slice(0).toLowerCase()}!`;
