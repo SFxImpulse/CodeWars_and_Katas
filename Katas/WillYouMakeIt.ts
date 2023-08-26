861ba666a3c5f0700aff7805a5464d6c87ce154c
David Braverman <braverman.david.business@gmail.com>
2023-08-26 08:59:56 -0400

    add another kata

---
+++ Katas/WillYouMakeIt.ts
@@ -0,0 +1,3 @@
+// Given 3 integers, determine whether you the factor of the second and third integers is greater than the first integer:
+
+const zeroFuel = (distance: number, mpg: number, fuelLeft: number): boolean => fuelLeft * mpg >= distance;
