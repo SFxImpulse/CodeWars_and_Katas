71899bc7fee6f5b3d8fdff5372dfb42fbf1ea320
David Braverman <braverman.david.business@gmail.com>
2023-09-12 21:24:36 -0400

    add another kata

---
+++ Katas/Calculate.py
@@ -0,0 +1,15 @@
+# Write mathemtical operation functions:
+
+import math
+
+add = lambda x, y: x + y
+
+subtract = lambda x, y: x - y
+
+multiply = lambda x, y: x * y
+
+divide = lambda x, y: x / y
+
+square = lambda x, y: x ** y
+
+square_root = lambda x: math.sqrt(x)
